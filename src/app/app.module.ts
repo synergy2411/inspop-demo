8accf2e4ccd6cd9d92a41d668f3a81deadf5d52e
synergy2411 <synergy2411@outlook.com>
2018-06-29 16:17:31 +0530

    Reactive Form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -10,6 +10,7 @@ import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
 import { CountryCodePipe } from './pipes/countrycode.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 
 @NgModule({
@@ -21,12 +22,14 @@ import { LoginComponent } from './auth/login/login.component';
     PipeDemoComponent,
     CountryCodePipe,
     FilterPipe,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
 
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  username = new FormControl('',[
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  password = new FormControl('',[
+    Validators.required,
+    this.forExclamationMark
+  ]);
+  registerForm : FormGroup;
+
+  forExclamationMark(input : FormControl){
+    const hasExcl = input.value.indexOf('!') >= 0;
+    return hasExcl ? null : { 'needExclamation' : true}
+  }
+
+  register(){
+    console.log(this.registerForm);
+  }
+  constructor(private fb : FormBuilder) { 
+    this.registerForm = this.fb.group({
+      username : this.username,
+      password : this.password
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
