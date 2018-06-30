8f73642cf40fb6d2b35204af52df80933e0c8190
synergy2411 <synergy2411@outlook.com>
2018-06-30 13:04:29 +0530

    final commit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  changeDetection : ChangeDetectionStrategy.
 })
 export class ProductComponent  {
   id : number; 
   name : string;
   page_no : number;
-  constructor(private route : ActivatedRoute) { 
+  dangerUrl : string = "javascript:alert('Hello! I am NOT Safe!')";
+  safeUrl : any;
+  constructor(private route : ActivatedRoute,
+            private sanitized : DomSanitizer,
+            private cdRef : ChangeDetectorRef,
+            private ngZone : NgZone) {
+              // this.cdRef.
+              this.ngZone.runOutsideAngular(()=>{
+                
+              })
+              this.safeUrl = this.sanitized.bypassSecurityTrustUrl(this.dangerUrl);
     // this.id = this.route.snapshot.params['id'];
     // this.name = this.route.snapshot.params['name'];
     
